2b947c821829f67229fe6835ab23bb941a557a74
Brendon <425026+brendon-codes@users.noreply.github.com>
2013-01-05 18:18:04 -0800

    Moving bin files

--- src/4/Euler4.hs
+++ src/4/Euler4.hs
@@ -27,30 +27,38 @@ module Main (
 ) where 
 
 
+nums :: Int -> [Int]
 nums 1 = [1]
 nums 2 = [2]
 nums n = [n,n-1..1]
 
 
+builder :: Int -> [Int]
 builder x = map (*x) (nums x)
 
 
+mults :: Int -> Int -> [[Int]]
 mults y z = map builder [z,z-1..y]
 
 
+allMults :: Int -> Int -> [Int]
 allMults y z = concat (mults y z)
 
 
+eachMult :: Int -> Bool
 eachMult m = (reverse s) == s
   where s = show m
 
 
+filtered :: Int -> Int -> [Int]
 filtered y z = filter eachMult (allMults y z)
 
 
+grab :: Int -> Int -> Int
 grab y z = maximum (filtered y z)
 
 
+solve :: Int
 solve = grab y z
   where y = 100
         z = 999
