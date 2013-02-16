ba34b201a626f942e2f3ecdc9cf630420f0c9084
Brendon <425026+brendon-codes@users.noreply.github.com>
2013-02-16 11:15:04 -0800

    Finished

--- src/017/Euler17.hs
+++ src/017/Euler17.hs
@@ -33,6 +33,10 @@ module Main (
 ) where
 
 
+--
+-- Ones places
+--
+ones :: [String]
 ones = ["one",
         "two",       "three",
         "four",      "five",
@@ -45,26 +49,53 @@ ones = ["one",
         "eighteen",  "nineteen"]
 
 
+--
+-- Tens places
+--
+tens :: [String]
 tens = ["twenty", "thirty",
         "forty",  "fifty",
         "sixty",  "seventy",
         "eighty", "ninety"]
 
+
+--
+-- And
+--
+andd :: String
 andd = "and"
+
+--
+-- Hundred
+--
+hun :: String
 hun = "hundred"
+
+
+--
+-- Thousand
+--
+thous :: String
 thous = "thousand"
 
 
+--
+-- Build a string given value
+--
 comp :: Int -> String
 comp x
   | (x < 1) =
     error "Value must be greater than zero"
   | (x < 20) =
     ones !! (x - 1)
+  | (x < 100) && (x `mod` 10 == 0) =
+    (tens !! ((x `div` 10) - 2))
   | (x < 100) =
     (tens !! ((x `div` 10) - 2)) ++ (rec x)
   | (x == 100) =
     (ones !! 0) ++ hun
+  | (x < 1000) && (x `mod` 100 == 0) =
+    (ones !! ((x `div` 100) - 1)) ++ hun
   | (x < 1000) =
     (ones !! ((x `div` 100) - 1)) ++ hun ++ andd ++ (rec x)
   | (x == 1000) =
@@ -76,10 +107,28 @@ comp x
 
 
 --
+-- Build list of strings
+--
+builder :: Int -> Int -> [String]
+builder start end = map comp [start..end]
+
+
+--
+-- Build sums of all results
+--
+getSums :: Int -> Int -> Int
+getSums start end =
+  sum $ map length $ builder start end
+
+
+--
 -- Solve
 --
 solve :: Int
-solve = 1
+solve = getSums start end
+  where
+    start = 1
+    end = 1000
 
 
 --
